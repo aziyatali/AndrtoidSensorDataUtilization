28818bd8b0e78b0d2b66f79f82db79f52e514924
Aziyat <aziyatalik4@gmail.com>
2021-11-23 02:28:20 +0900

    Gyroscope Implemented

--- app/src/main/java/com/example/sensor_accelerometer/MainActivity.java
+++ app/src/main/java/com/example/sensor_accelerometer/MainActivity.java
@@ -7,47 +7,73 @@ import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity{
 
     private TextView textView;
     private SensorManager sensorManager;
-    private Sensor sensor;
-//    int isClicked = 0;
+    private Sensor sensor, gyroscope;
+    SensorEventListener sensorEventListener, gyroscopeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         textView = findViewById(R.id.text_accelerometer);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        // sensor for accelerometer
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(MainActivity.this, sensor, sensorManager.SENSOR_DELAY_NORMAL);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        if (sensor == null){
+            Toast.makeText(this, "value is null", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        if (gyroscope == null){
+            Toast.makeText(this, "value is null", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float x = event.values[0], y = event.values[1], z = event.values[2];
+                textView.setText(x + "\n" + y + "\n" + z);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        gyroscopeListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float x = event.values[0], y = event.values[1], z = event.values[2];
+                textView.setText("Gyroscope: "+ x + "\n" + y + "\n" + z);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
 
     }
-
-    // Change the values of x, y, z onSensor
     @Override
-    public void onSensorChanged(SensorEvent event){
-        float x = event.values[0], y = event.values[1], z = event.values[2];
-        textView.setText(x + "\n" + y + "\n" + z);
-    }
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroscopeListener, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
+    }
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
-
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(gyroscopeListener);
+        sensorManager.unregisterListener(sensorEventListener);
     }
-
-//    public void onClick(View view){
-//        isClicked++;
-//        isClicked%=2;
-//        TextView text = findViewById(R.id.text_accelerometer);
-//    }
-
-
-
 
 }
