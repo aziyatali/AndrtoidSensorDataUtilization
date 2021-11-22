8f0dbde25a53d442dd909e1949ebbaf6e741d542
Aziyat <aziyatalik4@gmail.com>
2021-11-23 03:23:49 +0900

    Graph added for accelerometer and gyroscope

--- app/src/main/java/com/example/sensor_accelerometer/MainActivity.java
+++ app/src/main/java/com/example/sensor_accelerometer/MainActivity.java
@@ -10,6 +10,10 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends AppCompatActivity{
 
     private TextView textView;
@@ -60,6 +64,17 @@ public class MainActivity extends AppCompatActivity{
 
             }
         };
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
 
     }
     @Override
